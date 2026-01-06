f64ac15047dfc58e4b197ff0a9dee54d618dc7a2
Mitch Ryan <mitch@diesdas.digital>
2026-01-06 15:55:17 +0100

    Add access to np config

--- .np-config.mjs
+++ .np-config.mjs
@@ -1,4 +1,5 @@
 export default {
+  access: "public",
   // With Yarn Berry, `yarn npm publish` doesn't accept a directory argument.
   // Publish from the repo root and rely on `package.json#files` to include `dist/`.
 };
