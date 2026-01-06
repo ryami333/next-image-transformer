7a7f73180ccebfce815023dfac37e213eed50edc
Mitch Ryan <mitch@diesdas.digital>
2026-01-06 15:56:39 +0100

    Set registry in np config

--- .np-config.mjs
+++ .np-config.mjs
@@ -1,5 +1,6 @@
 export default {
   access: "public",
+  registry: "https://registry.npmjs.org",
   // With Yarn Berry, `yarn npm publish` doesn't accept a directory argument.
   // Publish from the repo root and rely on `package.json#files` to include `dist/`.
 };
